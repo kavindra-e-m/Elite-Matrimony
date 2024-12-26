2860010f87cb429adfda8b46b2f2312bb16bffc4
monhit raju <monhitrajul@gmail.com>
2024-12-26 23:20:33 +0530

    final from gender selection page

--- women/script.js
+++ women/script.js
@@ -7,3 +7,17 @@ function showDetails(profileNumber) {
 function hideDetails(profileNumber) {
     document.getElementById("profile-details" + profileNumber).style.display = "none";
 }
+
+// Function to initiate a Google Meet call (both voice and video)
+function initiateGoogleMeetCall() {
+    const googleMeetURL = "https://meet.google.com/xyz-abc-def"; // Replace with your actual Google Meet URL
+    window.location.href = googleMeetURL;
+}
+// Redirect to subscription.html when the buttons are clicked
+document.querySelector('.voice-call').onclick = function() {
+    window.location.href = 'SUB.html';
+};
+
+document.querySelector('.video-call').onclick = function() {
+    window.location.href = 'SUB.html';
+};
